e939418630984373eeb27291f515d3f5032471d1
aditpgnSkom <littlecommander46@gmail.com>
2024-11-05 20:32:00 +0700

    add: register

--- Django/skripsi_plus/public_app/views.py
+++ Django/skripsi_plus/public_app/views.py
@@ -3,30 +3,38 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 
-# Create your views here.
+
 def home(request):
     return render(request, 'index.html')
+
 
 def about(request):
     return render(request, 'about.html')
 
+
 def faq(request):
     return render(request, 'faq.html')
 
+
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username_or_email = request.POST.get('username_or_email')
         password = request.POST.get('password')
-        user = authenticate(request,username=username, password=password)
 
+        try:
+            user = User.objects.get(email=username_or_email)
+            username = user.username
+        except User.DoesNotExist:
+            username = username_or_email
+
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            # nanti redirect dashboard user
-            return redirect('home')
+            return redirect('home')  # jika login berhasil
         else:
             messages.error(request, 'Username atau password salah')
-            return redirect('login')
     return render(request, 'login.html')
+
 
 def register_view(request):
     if request.method == 'POST':
@@ -34,21 +42,24 @@ def register_view(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         confirm_password = request.POST.get('confirm_password')
-        
+
         if password == confirm_password:
             if User.objects.filter(username=username).exists():
                 messages.error(request, 'Username sudah digunakan')
             elif User.objects.filter(email=email).exists():
                 messages.error(request, 'Email sudah digunakan')
             else:
-                user = User.objects.create_user(username=username, email=email, password=password)
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
                 user.save()
-                messages.success(request, 'Registrasi berhasil! Silahkan login.')
+                messages.success(
+                    request, 'Registrasi berhasil! Silahkan login.')
                 return redirect('login')
         else:
             messages.error(request, 'Password tidak sesuai')
     return render(request, 'register.html')
 
+
 def logout_view(request):
     logout(request)
-    return redirect('')
+    return redirect('home')
