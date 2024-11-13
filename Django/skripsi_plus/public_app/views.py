f241fdc0c5b302b20a77a6e2dc982704409acf50
aditpgnSkom <littlecommander46@gmail.com>
2024-11-13 20:28:18 +0700

    add: login_required on course_detail

--- Django/skripsi_plus/public_app/views.py
+++ Django/skripsi_plus/public_app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from .models import Mentor, Course
 from django.db.models import Q
-
 
 
 def home(request):
@@ -19,7 +19,56 @@ def faq(request):
     return render(request, 'faq.html')
 
 
+def mentors(request):
+    # print("GET data:", request.GET)
+    query = request.GET.get('query')  # Ambil nilai pencarian dari URL
+    # print("Query yang diterima:", query)
+    mentors = Mentor.objects.all()
+
+    if query:
+        # Filter berdasarkan nama, rating, atau skill menggunakan Q objects
+        mentors = mentors.filter(
+            Q(name__icontains=query) |
+            Q(rating__icontains=query) |
+            Q(skills__icontains=query)
+        ).distinct()  # distinct() menghindari hasil duplikasi
+
+    for mentor in mentors:
+        # Memisahkan education berdasarkan '\n' agar menjadi list
+        mentor.education = mentor.education.split("\n")
+        mentor.skills = mentor.skills.split(",")
+
+    return render(request, 'mentors.html', {'mentors': mentors})
+
+
+def course(request):
+    query = request.GET.get('query')
+    courses = Course.objects.all()
+
+    if query:
+        # Filter courses based on name or price using Q objects
+        courses = courses.filter(
+            Q(title__icontains=query) |
+            Q(price__icontains=query)
+        ).distinct()  # Use distinct() to avoid duplicate results
+
+    return render(request, 'courses.html', {'courses': courses})
+
+
+@login_required(login_url='login')
+def course_detail(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    return render(request, 'detail-courses.html', {'course': course})
+
+
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if 'next' in request.GET:
+        messages.info(
+            request, 'Silahkan login untuk mengakses halaman tersebut.')
+
     if request.method == 'POST':
         username_or_email = request.POST.get('username_or_email')
         password = request.POST.get('password')
@@ -40,6 +89,9 @@ def login_view(request):
 
 
 def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         email = request.POST.get('email')
         username = request.POST.get('username')
@@ -66,42 +118,3 @@ def register_view(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
-
-def mentors(request):
-    # print("GET data:", request.GET)
-    query = request.GET.get('query')  # Ambil nilai pencarian dari URL
-    # print("Query yang diterima:", query)
-    mentors = Mentor.objects.all()
-
-    if query:
-        # Filter berdasarkan nama, rating, atau skill menggunakan Q objects
-        mentors = mentors.filter(
-            Q(name__icontains=query) |
-            Q(rating__icontains=query) |
-            Q(skills__icontains=query)
-        ).distinct()  # distinct() menghindari hasil duplikasi
-    
-    for mentor in mentors:
-        # Memisahkan education berdasarkan '\n' agar menjadi list
-        mentor.education = mentor.education.split("\n")
-        mentor.skills = mentor.skills.split(",")
-    
-    
-    return render(request, 'mentors.html', {'mentors': mentors})
-
-def course(request):
-    query = request.GET.get('query') 
-    courses = Course.objects.all()
-
-    if query:
-        # Filter courses based on name or price using Q objects
-        courses = courses.filter(
-            Q(title__icontains=query) |
-            Q(price__icontains=query)
-        ).distinct()  # Use distinct() to avoid duplicate results
-    
-    return render(request, 'courses.html', {'courses': courses})
-
-def course_detail(request, pk):
-    course = get_object_or_404(Course, pk=pk)
-    return render(request, 'detail-courses.html', {'course': course})
